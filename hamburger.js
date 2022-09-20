e64ea6f1754d1b428a5c857f10ef3854eccefed0
Olivier-Kango <olivierkango@gmail.com>
2022-09-20 00:32:40 -0700

    Add the About page with the logo Kango

--- hamburger.js
+++ hamburger.js
@@ -1,12 +1,10 @@
 const hamburger = document.querySelector('.container_menu');
 const navMenu = document.querySelector('.menu');
-const sup = document.querySelector('.main-title');
 const fixedHamburger = document.querySelector('.div-home');
 
 const bars = () => {
   hamburger.classList.toggle('change');
   navMenu.classList.toggle('change');
-  sup.classList.toggle('change');
   fixedHamburger.classList.toggle('change');
 };
 
@@ -15,6 +13,5 @@ hamburger.addEventListener('click', bars);
 document.querySelectorAll('.nav-link').forEach((element) => element.addEventListener('click', () => {
   hamburger.classList.remove('change');
   navMenu.classList.remove('change');
-  sup.classList.remove('change');
   fixedHamburger.classList.remove('change');
 }));
