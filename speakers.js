62411b7e1ab6809aac91f60710b1e46d0d34179f
Olivier-Kango <olivierkango@gmail.com>
2022-09-21 11:16:37 -0700

    create the speakers.js file

---
+++ speakers.js
@@ -0,0 +1,11 @@
+// h2.title-speakers
+// div.underline
+
+// all the speakers in a div.div-speakers => flex culomn {
+  // each speaker in a div.featured-speaker => flex row {
+    // one div.image-speaker
+    // one div.content-speaker ={
+      // Name, line (div), Profession, About.
+    // } 
+  // } 
+// }
